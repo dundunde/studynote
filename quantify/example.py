5a4b7b4ae149d46a2376d6b69276f1712b3fad42
dun <dun@dun.com>
2026-03-03 17:31:33 +0800

    备份

---
+++ quantify/example.py
@@ -0,0 +1,77 @@
+﻿import datetime  #For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+import backtrader as bt
+
+class TestStrategy(bt.Strategy):
+    params = (
+        ('maperiod', 15),
+        ('printlog', False),
+    )
+
+    def log(self, txt, dt=None, doprint=False):
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+        self.sma = bt.indicators.MovingAverageSimple(
+            self.datas[0], period=self.params.maperiod)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:
+                self.bar_executed = len(self)
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+    def next(self):
+        if self.order:
+            return
+
+        if not self.position:
+            if self.dataclose[0] > self.sma[0]:
+                self.order = self.buy()
+        else:
+            if self.dataclose[0] < self.sma[0]:
+                self.order = self.sell()
+
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.maperiod, self.broker.getvalue()), doprint=True)
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+    strats = cerebro.optstrategy(
+        TestStrategy,
+        maperiod=range(10, 31))
+
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'c:/Users/dun/Desktop/studynote/studynote/quantify/data/orcl-1995-2014.txt')
+
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath,
+        fromdate=datetime.datetime(2000, 1, 1),
+        todate=datetime.datetime(2000, 12, 31),
+        reverse=False)
+
+    cerebro.adddata(data)
+    cerebro.broker.setcash(1000.0)
+    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+    cerebro.broker.setcommission(commission=0.0)
+
+    cerebro.run(maxcpus=1)
